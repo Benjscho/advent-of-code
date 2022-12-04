86edc96714aa594934508d1e72488ca0238b963b
Ben Schofield <me@benjscho.dev>
2022-12-03 21:26:26 -0800

    Day 4
    Completed day four. This one was quite nice, opportunity to parse some
    simply formatted strings. Rust has very nice string parsing for this
    kind of thing. I like split_once for this kind of formatted data.
    
    Another good use for a fold too.

---
+++ day4/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let result = input.lines().fold(0, |x, pair| x + full_overlap(pair)); 
+    println!("Number of completely overlapping pairs: {}", result); 
+    let result = input.lines().fold(0, |x, pair| x + any_overlap(pair)); 
+    println!("Number of partially overlapping pairs: {}", result); 
+}
+
+fn full_overlap(s: &str) -> u32 {
+    let (a, b) = s.split_once(',').unwrap();
+    let (sa, ea) = a.split_once('-').unwrap();
+    let (sb, eb) = b.split_once('-').unwrap();
+    let sa = sa.parse::<u32>().unwrap();
+    let sb = sb.parse::<u32>().unwrap();
+    let ea = ea.parse::<u32>().unwrap();
+    let eb = eb.parse::<u32>().unwrap();
+
+    if (sa <= sb && ea >= eb) || 
+        (sb <= sa && eb >= ea) {
+        1
+    } else {
+        0
+    }
+}
+
+fn any_overlap(s: &str) -> u32 {
+    let (a, b) = s.split_once(',').unwrap();
+    let (sa, ea) = a.split_once('-').unwrap();
+    let (sb, eb) = b.split_once('-').unwrap();
+    let sa = sa.parse::<u32>().unwrap();
+    let sb = sb.parse::<u32>().unwrap();
+    let ea = ea.parse::<u32>().unwrap();
+    let eb = eb.parse::<u32>().unwrap();
+
+    // Pair doesn't overlap when the end of the first section is before
+    // the start of the other. 
+    if ea < sb || sa > eb {
+        0
+    } else {
+        1
+    }
+}
