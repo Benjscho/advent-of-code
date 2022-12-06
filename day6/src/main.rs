ef440386221b0cfd541f3045787037ace5d68bbd
Ben Schofield <me@benjscho.dev>
2022-12-05 21:33:51 -0800

    Day 6
    This one was pretty simple to implement, although I don't know how
    rust-y my code is. I found fasterthanlime's series on AoC today, so I'll
    be reading those to compare how my code could be better!
    
    I write this one without the marker_len to begin with, but it was very
    easy to slot in to fix part two of the puzzle, just extending the
    marker length from 4 to 14. Fun little puzzle. The string function for
    removing the 0th value is O(n), which makes the get_marker func O(n*k)
    where n is the input length and k is the length of the unique character
    string. It could be improved by using a deque instead of a string there.
    Not too sure about the is_marker function, it'll instantiate a HashSet
    each time which is a little more expensive computationally, but in terms
    of verifying if a string contains only unique chars it is O(n). Actually
    that makes this solution O(n*k*k) - switching to a deque (giving O(1)
    string editing) would make this O(n*k) overall.

---
+++ day6/src/main.rs
@@ -0,0 +1,38 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = include_str!("input.txt");
+    println!("The message is {} chars long", input.len());
+
+    let result = get_marker(input, 4);
+    println!("The start of packet marker is detected at {}", result);
+    let result = get_marker(input, 14);
+    println!("The start of message marker is detected at {}", result);
+}
+
+fn get_marker(message: &str, marker_len: usize) -> usize {
+    let (potential_marker, remainder) = message.split_at(marker_len);
+    let mut pm = potential_marker.to_string();
+    let mut pos = marker_len;
+    for c in remainder.chars() {
+        if is_marker(&pm) {
+            return pos
+        }
+        pm.remove(0);
+        pm.push(c);
+        pos += 1; 
+    }
+    pos
+}
+
+fn is_marker(pm: &str) -> bool {
+    let mut a: HashSet<char> = HashSet::new();
+    for c in pm.chars() {
+        if a.contains(&c) {
+            return false
+        } else {
+            a.insert(c);
+        }
+    }
+    true
+}
