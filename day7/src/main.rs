e60bd47b53f01ee6fc3c796462098022dffe0bcb
Ben Schofield <me@benjscho.dev>
2022-12-07 11:06:59 -0800

    File and Dir parse individually

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -12,6 +12,9 @@ fn main() {
     println!("Hello, world!");
 }
 
+
+
+// -------- PARSING SECTION -------- //
 fn parse_number(i: &str) -> IResult<&str, u64> {
     map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
         s.parse::<u64>()
@@ -19,47 +22,107 @@ fn parse_number(i: &str) -> IResult<&str, u64> {
 }
 
 fn parse_name(i: &str) -> IResult<&str, &str> {
-    let (i, name) = take_while1(|c: char| c.is_alphanumeric())(i)?;
+    let (i, name) = take_while1(|c: char| !c.is_whitespace())(i)?;
     Ok((i, name))
 }
 
-fn parse_file(i: &str) -> IResult<&str, File> {
-    let (input, (size, _, name)) = tuple((parse_number, tag(" "), parse_name))(i)?;
-    let name = name.to_string();
+impl File {
+    fn parse(i: &str) -> IResult<&str, File> {
+        let (input, (size, _, name)) = tuple((parse_number, tag(" "), parse_name))(i)?;
+        let name = name.to_string();
 
-    Ok((input, File { size, name }))
+        Ok((input, File { size, name }))
+    }
 }
 
-fn parse_dir(i: &str) -> IResult<&str, Dir> {
-    let (i, _) = tag("dir ")(i)?;
-    let (i, name) = take_while1(|c: char| c.is_alphanumeric())(i)?;
+impl Dir {
+    fn parse(i: &str) -> IResult<&str, Dir> {
+        let (i, _) = tag("dir ")(i)?;
+        let (i, name) = take_while1(|c: char| c.is_alphanumeric())(i)?;
 
-    Ok((i, Dir {total_size: 0, objects: HashMap::new(), name: name.to_string()}))
+        Ok((i, Dir {total_size: 0, objects: HashMap::new(), name: name.to_string()}))
+    }
 }
 
-//fn parse_command(i: &str) -> IResult<&str, 
+impl Move {
+    fn parse(i: &str) -> IResult<&str, Move> {
+        let (i, _) = tag("cd ")(i)?;
+        let (i, target) = take_while1(|c: char| !c.is_whitespace())(i)?;
+        Ok((i, Move {target: target.to_string()}))
+    }
+}
+
 
 //fn parse_file_or_dir(i: &str) -> IResult<&str, Option<FileObject>> {
 //    alt((map(parse_file, Some), map(parse_dir, Some)))(i)
 //}
 
 // ---- Types
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 enum FileObject {
     File,
     Dir
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct File {
     size: u64,
     name: String
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct Dir {
     total_size: u64,
     objects: HashMap<String, FileObject>,
     name: String
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum Command {
+    Move,
+    List
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Move {
+    target: String
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_file_parse() {
+        let file_string = "14848514 b.txt";
+        let file = File {size: 14848514, name: "b.txt".to_string()};
+
+        let (_, parsed_file) = File::parse(file_string).unwrap();
+        assert_eq!(file, parsed_file);
+    }
+
+    #[test]
+    fn test_dir_parse() {
+        let dir_string = "dir a";
+        let actual = Dir {total_size: 0, objects: HashMap::new(), name: "a".to_string()}; 
+        let (_, expected) = Dir::parse(dir_string).unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_move_parse() {
+        let move_strings = vec!["cd ..", "cd /", "cd e"];
+        let actual = vec![
+            Move {target: "..".to_string()},
+            Move {target: "/".to_string()},
+            Move {target: "e".to_string()},
+        ];
+
+        for (s, m) in move_strings.iter().zip(actual.iter()) {
+            let (_, tm) = Move::parse(s).unwrap();
+            assert_eq!(m, &tm); 
+        }
+    }
+}
