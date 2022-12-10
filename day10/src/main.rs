448a14c73c4fe988f3b60f774ca75dade8fffb40
Ben Schofield <me@benjscho.dev>
2022-12-10 12:15:16 -0800

    Day 10
    This is a really fun puzzle idea! Loved the drawing out of pixels part.
    
    As seems to be traditional for me so far, the parsinmg took the longest
    part. Even though this is incredibly simple, I wanted to give using
    `nom` a go again, but that backfired as I struggled to parse the add
    instruction. Found it very difficult to debug, couldn't get it workjing
    or figure out where I was going wrong. In the end I went back to very
    simple split on whitespace parsing, which worked absolutely fine!
    
    My logic for thje instruction handling is pretty simple. I created a
    deque for the instructions, and a temporary register for the current
    instruction. If the current instruction is empty I load a new one into
    it (noops count as empty instructions)/. If it's not empty I add the
    number to the X register.
    
    Logic for drawing the pixels is similarly simple. Good fun puzzle!

---
+++ day10/src/main.rs
@@ -0,0 +1,118 @@
+use std::collections::VecDeque;
+
+fn main() {
+    let input = include_str!("input.txt");
+    let res = solve_part1(input);
+    dbg!(res);
+    let screen_display = solve_part2(input);
+
+    println!("{}", screen_display);
+}
+
+fn solve_part1(input: &str) -> i64 {
+    let mut res = 0;
+    let mut ops: VecDeque<Option<i64>> = VecDeque::new();
+
+    for l in input.lines() {
+        let s = l.split_once(" "); 
+
+        match s {
+            Some((_, b)) => {
+                ops.push_back(Some(b.parse::<i64>().unwrap()))
+            },
+            _ => ops.push_back(None)
+        }
+    };
+
+    let impt_cycles = vec![20, 60, 100, 140, 180, 220]; 
+
+    let mut x = 1;
+    let mut temp: Option<i64> = None;
+    for i in 1..=220 { 
+        if impt_cycles.contains(&i) {
+            res += i*x;
+        }
+        match temp {
+            Some(val) => {
+                x += val;
+                temp = None;
+            }, 
+            None => {
+                let instruction = ops.pop_front();
+                match instruction {
+                    Some(x) => temp = x,
+                    _ => ()
+                }
+            }
+        }
+    }
+
+    res
+}
+
+fn solve_part2(input: &str) -> String {
+    let mut ops: VecDeque<Option<i64>> = VecDeque::new();
+
+    for l in input.lines() {
+        let s = l.split_once(" "); 
+
+        match s {
+            Some((_, b)) => {
+                ops.push_back(Some(b.parse::<i64>().unwrap()))
+            },
+            _ => ops.push_back(None)
+        }
+    };
+
+
+    let mut res_vec: Vec<char> = vec![];
+    let mut x = 1;
+    let mut temp: Option<i64> = None;
+    for i in 0..240 { 
+        let pixel = i % 40;
+        if pixel == 0 && i != 0 {
+            res_vec.push('\n');
+        }
+        if x - 1 == pixel || x == pixel || x + 1 == pixel {
+            res_vec.push('#');
+        } else {
+            res_vec.push('.');
+        }
+        match temp {
+            Some(val) => {
+                x += val;
+                temp = None;
+            }, 
+            None => {
+                let instruction = ops.pop_front();
+                match instruction {
+                    Some(x) => temp = x,
+                    _ => ()
+                }
+            }
+        }
+    }
+    res_vec.push('\n');
+
+    res_vec.iter().cloned().collect::<String>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    static TEST_INPUT: &str = include_str!("test-input.txt");
+    static TEST_OUTPUT: &str = include_str!("test-output.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(solve_part1(TEST_INPUT), 13140);
+    }
+
+    #[test]
+    fn test_part2() {
+        env::set_var("RUST_BACKTRACE", "1");
+        assert_eq!(solve_part2(TEST_INPUT), TEST_OUTPUT);
+    }
+}
