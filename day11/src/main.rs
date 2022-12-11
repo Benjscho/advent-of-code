b6615c74cfb8044c046b5fd50d01cf4155c3f9fc
Ben Schofield <me@benjscho.dev>
2022-12-11 09:19:48 -0800

    Day 11 complete
    This was so fun! I really enjoyed the part two problem. My immediate
    reaction was to just try increasing the size of the numbers, going for
    bigger and bigger ints. Really love how easy Rust makes that
    experimentation and how FAST it is, immediate turn around. I feel for
    the people today using python for this that probably sat there waiting
    for their code to run as it created larger and larger numbers until the
    stack overflowed.
    
    After stepping away for a second I realised the clue to find some other
    way to decrease your worry. Figured it out in the shower, really
    satisfying to realise there's a mathematical solution. Here it was to
    find a common multiple of the monkeys divisors (I just multiplied them
    as I didn't want to code a lowest common multiplier function). That
    meant the behaviour of the logic checks is consistent as the checks
    operate in a field (or is it a ring, not perfect at my number theory).
    
    Great puzzle, as always parsing took me an age, starting to get their
    with nom parsing though! My method is now lots of debug statements while
    I'm coding it and to just grab whatever I can with let statements. It's
    not the cleanest but it works for these very short parsing problems.

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -6,6 +6,8 @@ fn main() {
     let input = include_str!("input.txt");
     let res = solve_part1(input); 
     dbg!(res);
+    let res = solve_part2(input); 
+    dbg!(res);
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -31,12 +33,28 @@ fn solve_part1(i: &str) -> i64 {
     checks[0] * checks[1]
 }
 
+fn solve_part2(i: &str) -> i64 {
+    let mut monkeys: Vec<Monkey> = i.split("\n\n").map(
+        |s: &str| parse_monkey(s).unwrap().1
+    ).collect(); 
+
+    let common_divisor: i64 = monkeys.iter().fold(1, |a, m| a * m.division_test); 
+
+    for _ in 0..10000 {
+        one_round_part2(&mut monkeys, common_divisor);
+    }
+
+    let mut checks: Vec<i64> = monkeys.iter().map(|m: &Monkey| m.item_checks).collect();
+    checks.sort_by(|a,b| b.cmp(a));
+    checks[0] * checks[1]
+}
+
 fn one_round(monkeys: &mut Vec<Monkey>) {
     for i in 0..monkeys.len() {
         while !monkeys[i].items.is_empty() {
             monkeys[i].item_checks += 1;
             let item = monkeys[i].items.pop_front();
-            let nw = next_worry_level(item.unwrap(), monkeys[i].operation) / 3; 
+            let nw = next_worry_level(item.unwrap(), monkeys[i].operation) / 3;
             let next_monkey = monkeys[i].next_monkey;
             if nw % monkeys[i].division_test == 0 {
                 monkeys[next_monkey.0].items.push_back(nw);
@@ -46,6 +64,23 @@ fn one_round(monkeys: &mut Vec<Monkey>) {
         }
     }
 }
+
+fn one_round_part2(monkeys: &mut Vec<Monkey>, common_worry: i64) {
+    for i in 0..monkeys.len() {
+        while !monkeys[i].items.is_empty() {
+            monkeys[i].item_checks += 1;
+            let item = monkeys[i].items.pop_front();
+            let nw = next_worry_level(item.unwrap(), monkeys[i].operation) % common_worry;
+            let next_monkey = monkeys[i].next_monkey;
+            if nw % monkeys[i].division_test == 0 {
+                monkeys[next_monkey.0].items.push_back(nw);
+            } else {
+                monkeys[next_monkey.1].items.push_back(nw);
+            }
+        }
+    }
+}
+
 
 fn next_worry_level(worry: i64, op: (char, Option<i64>)) -> i64 {
     match op.0 {
@@ -128,6 +163,11 @@ mod test {
     #[test]
     fn test_solve_part1() {
         assert_eq!(10605, solve_part1(TEST_INPUT)); 
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        assert_eq!(2713310158, solve_part2(TEST_INPUT)); 
     }
 
     #[test]
